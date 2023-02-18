21d81612893fb07a98276e29adebcca2b11c0820
Hassan <kkromah12@gmail.com>
2023-02-18 15:17:53 +0300

    done

--- dist/server.js
+++ dist/server.js
@@ -1,16 +1,22 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
-import "express-async-errors";
-import morgan from "morgan";
-import { getAll, getUser, getPost, getUpdate, getDelete } from "./controllers/planets.js";
+import 'express-async-errors';
+import morgan from 'morgan';
+dotenv.config();
 const app = express();
-const port = 5000;
-app.use(morgan("dev"));
+const port = process.env.PORT;
+app.use(morgan('dev'));
 app.use(express.json());
-app.get("/api/planets", getAll);
-app.get("/api/planets/:id", getUser);
-app.post("/api/planets", getPost);
-app.put("/api/planets/:id", getUpdate);
-app.delete("/api/planets/:id", getDelete);
+let planets = [
+    { id: 1, name: 'Earth' },
+    { id: 2, name: 'Mars' },
+    { id: 3, name: 'Juputer' },
+    { id: 4, name: 'Plut' },
+];
+app.get('/', (req, res) => {
+    console.log(req);
+    res.status(200).json(planets);
+});
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
+    console.log(`Server is listening on port http://localhost:${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,36 @@
-import express
-from 'express';
-import "express-async-errors";
-import morgan from "morgan";
-import {getAll, getUser, getPost, getUpdate, getDelete} from "./controllers/planets.js"
+import * as dotenv from 'dotenv';
+import express from 'express';
+import 'express-async-errors';
+import morgan from 'morgan';
 
-const app = express()
-const port = 5000
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 
+// create Express App
+type Planet = {
+	id: number;
+	name: string;
+};
 
-app.get("/api/planets", getAll);
+type Planets = Planet[];
 
-app.get("/api/planets/:id", getUser);
+let planets: Planets = [
+	{ id: 1, name: 'Earth'},
+	{ id: 2, name: 'Mars'},
+	{ id: 3, name: 'Juputer'},
+	{ id: 4, name: 'Plut'},
+];
 
-app.post("/api/planets", getPost);
+app.get('/', (req, res) => {
+	console.log(req);
 
-app.put("/api/planets/:id", getUpdate);
-
-app.delete("/api/planets/:id", getDelete);
-
+	res.status(200).json(planets);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
-
-
-
-
-
-
+	console.log(`Server is listening on port http://localhost:${port}`);
+});
